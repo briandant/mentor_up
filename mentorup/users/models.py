3982061b54003e91936056ef8244efe9ebbd083b
chrisvans <chrisvanschyndel@gmail.com>
2013-09-28 15:48:33 -0400

    Cleaned up users Models and Methods, added Docstrings

--- mentorup/users/models.py
+++ mentorup/users/models.py
@@ -17,6 +17,9 @@ import select2.fields
 import select2.models
 
 class Skills(models.Model):
+    """
+    Model for storing skills and associating them with a user
+    """
     active = models.BooleanField(default=True)
     name = models.CharField(max_length=50)
     teach = models.BooleanField(default=False)
@@ -24,35 +27,60 @@ class Skills(models.Model):
     def __unicode__(self):
         return self.name + " Teach: " + str(self.teach)
 
-
-    # Call this method when initializing the available tags in the DB.
-    # It can be called safely multiple times without issue, as django-taggit checks against duplicates
     @classmethod
     def generate_skills(cls):
-        base_tags = ["Python", "Django", "Flask", "Ruby", "Ruby on Rails", "Javascript", "Node.js", "Angular", "Backbone", "Scala", "PHP", "Java", "HTML5", "CSS3", "Jquery"]
-        skill_level_tags = ["No Experience", "Beginner", "Intermediate", "Expert"]
-        for tag in base_tags:
-            for skill_tag in skill_level_tags:
-                tag_skill = "%s %s" %(tag, skill_tag)
-                tag = "%s" %(tag)
+        """
+        Call this method when initializing the available tags in the DB.
+        It can be called safely multiple times without issue, as it checks against duplicates
+        """
+        base_skills = ["Python", "Django", "Flask", "Ruby", "Ruby on Rails", "Javascript", "Node.js", "Angular", "Backbone", "Scala", "PHP", "Java", "HTML5", "CSS3", "Jquery"]
+        skill_levels = ["No Experience", "Beginner", "Intermediate", "Expert"]
+        for skill in base_skills:
+            for skill_level in skill_levels:
+                tag_skill = "%s %s" %(skill, skill_level)
+                tag = "%s" %(skill)
                 Skills.objects.get_or_create(name=tag, teach=False)
                 Skills.objects.get_or_create(name=tag_skill, teach=False)
                 Skills.objects.get_or_create(name=tag, teach=True)
                 Skills.objects.get_or_create(name=tag_skill, teach=True)
 
-    # Create a single tag with this method, if you specify a skill level ( which you should )
-    # It will also create the base tag.  I.E. Skills.create_tag("Python", "Expert")
-    # Will create the tag "Python" as well as the tag "Python Expert"
     @classmethod
-    def create_skill(cls, tag, skill_level=None, teach=False):
-        if not skill_level:
-            tag = "%s" %(tag)
-            Skills.objects.get_or_create(name=tag, teach=teach)
+    def create_skill(cls, skill, skill_level=None):
+        """
+        Create a single tag with this method, if you specify a skill level ( which you should )
+        It will also create the base skill.  I.E. Skills.create_skill("Python", "Expert")
+        will create the skill "Python" as well as the skill "Python Expert"
+        """
+        if skill_level:
+            tag_skill = "%s %s" %(skill, skill_level)
+            tag = "%s" %(skill)  
+            Skills.objects.get_or_create(name=tag, teach=False)
+            Skills.objects.get_or_create(name=tag_skill, teach=False)
+            Skills.objects.get_or_create(name=tag, teach=True)
+            Skills.objects.get_or_create(name=tag_skill, teach=True)
         else:
-            tag_skill = "%s %s" %(tag, skill_level)
-            tag = "%s" %(tag)  
-            Skills.objects.get_or_create(name=tag, teach=teach)
-            Skills.objects.get_or_create(name=tag_skill, teach=teach)
+            tag = "%s" %(skill)
+            Skills.objects.get_or_create(name=tag, teach=False)
+            Skills.objects.get_or_create(name=tag, teach=True)
+
+    @classmethod
+    def create_skills_levels(cls, skills, skill_levels=["No Experience", "Beginner", "Intermediate", "Expert"]):
+        """
+        Create a skill for each skill level, and the base skill.
+        Pass in multiple skills to add more than one ( expects an array either way ).
+        I.E. Skills.create_skill_levels("Python")
+        Will create the skills "Python", "Python No Experience", "Python Beginner", "Python Intermediate", "Python Expert"
+        Specify an array for skill_levels to input custom skill levels.
+        Checks for duplicates, and will only save unique values.
+        """
+        for skill in skills:
+            for skill_level in skill_levels:
+                tag_skill = "%s %s" %(skill, skill_level)
+                tag = "%s" %(skill)
+                Skills.objects.get_or_create(name=tag, teach=False)
+                Skills.objects.get_or_create(name=tag_skill, teach=False)
+                Skills.objects.get_or_create(name=tag, teach=True)
+                Skills.objects.get_or_create(name=tag_skill, teach=True)
 
 # Subclass AbstractUser
 class User(AbstractUser):
@@ -66,6 +94,10 @@ class User(AbstractUser):
 
 
 class Search(models.Model):
+    """
+    Model for hooking up available skill classes with ajax
+    search autocomplete requests.
+    """
     skill_categories = select2.fields.ForeignKey(Skills,
         limit_choices_to=models.Q(active=True),
         ajax=True,
