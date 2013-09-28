24015e81c1cc781498383a5b25e12b0dde166c12
chrisvans <chrisvanschyndel@gmail.com>
2013-09-28 00:48:39 -0400

    Setup Imports for Chosen and Taggit

--- mentorup/users/models.py
+++ mentorup/users/models.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
+# Import chosenforms for pretty search forms
+from chosen import forms as chosenforms
 # Import the AbstractUser model
 from django.contrib.auth.models import AbstractUser
 
-# Import the basic Django ORM models library
+# Import the basic Django ORM models and forms library
 from django.db import models
+from django import forms
+
+# Import tags for searching
+from taggit.models import Tag
+from taggit.models import TagBase
+from taggit.managers import TaggableManager
 
 from django.utils.translation import ugettext_lazy as _
 
