541464055da7930efa80149ac63a900018501ceb
Nate Aune <natea@jazkarta.com>
2013-10-02 21:31:53 -0400

    enable ddt in production

--- mentorup/config/settings.py
+++ mentorup/config/settings.py
@@ -334,6 +334,21 @@ class Production(Common):
     INSTALLED_APPS = Common.INSTALLED_APPS
     ########## END INSTALLED_APPS
 
+    MIDDLEWARE_CLASSES = Common.MIDDLEWARE_CLASSES + ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+    INSTALLED_APPS += ('debug_toolbar', )
+
+    INTERNAL_IPS = ('127.0.0.1',)
+
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': False,
+        'SHOW_TEMPLATE_CONTEXT': True,
+        'SHOW_TOOLBAR_CALLBACK': show_toolbar,
+    }
+
+    def show_toolbar(request):
+        #return request.user.is_staff
+        return True
+
     ########## SECRET KEY
     SECRET_KEY = values.SecretValue()
     ########## END SECRET KEY
